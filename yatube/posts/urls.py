3e9a072d4e44b2562db1eafca8d98f885467d56f
Магомед Муцольгов <maga.mutsolgov@bk.ru>
2023-02-07 00:45:31 +0300

    one version

---
+++ yatube/posts/urls.py
@@ -0,0 +1,12 @@
+# posts/urls.py
+from django.urls import path
+
+from . import views
+
+app_name = 'posts'
+# app_name = 'index'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('group/<slug:slug>/', views.group_posts, name='group_list'),
+]
